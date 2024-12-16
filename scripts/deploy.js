c17b875ca06e221c7905c34efb58dbffcb4c8374
ideasarethefuture <121265474+ideasarethefuture@users.noreply.github.com>
2024-12-16 16:25:59 -0600

    updated project to include hardhat and local testing

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+async function main() {
+    // Get the contract to deploy
+    const JobMarketplace = await ethers.getContractFactory("JobMarketplace");
+    console.log("Deploying JobMarketplace...");
+
+    // Deploy the contract
+    const jobMarketplace = await JobMarketplace.deploy();
+
+    // Wait for the deployment to complete
+    await jobMarketplace.deployed();
+
+    console.log(`JobMarketplace deployed to: ${jobMarketplace.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
